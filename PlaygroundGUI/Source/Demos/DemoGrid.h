ec146a9272a5cfdc207c314d75e9d6de1cd55084
Carrie <karonka11@gmail.com>
2019-11-27 16:39:38 +0200

    grid component update

--- PlaygroundGUI/Source/Demos/DemoGrid.h
+++ PlaygroundGUI/Source/Demos/DemoGrid.h
@@ -20,16 +20,9 @@
 #pragma once
 
 #include "../../JuceLibraryCode/JuceHeader.h"
+#include "../Components/ValidatorGrid.h"
 
 static const char* TEST_OWNER = "thisistestownerthisistestowner00";
-
-class SlotsModelInterface {
- public:
-  virtual ~SlotsModelInterface() {}
-  virtual uint32_t get_slots_number() = 0;
-  virtual std::string get_slot_owner(uint32_t slot_index) = 0;
-  virtual std::string get_slot_difficulty(uint32_t slot_index) = 0;
-};
 
 class SlotsModelTest: public SlotsModelInterface {
  public:
@@ -49,44 +42,15 @@ class SlotsModelTest: public SlotsModelInterface {
 
 class DemoGrid: public Component, private Timer {
  public:
-  DemoGrid(std::shared_ptr<SlotsModelInterface> model, const std::string& owner,
-      uint64_t initial_component_size, bool allow_text_over_slots);
+  DemoGrid(std::shared_ptr<SlotsModelInterface> model, const std::string& owner);
   ~DemoGrid();
 
-  void paint(Graphics& g) override;
   void resized() override;
   void update();
 
-  void set_owner(const std::string& new_owner);
-  void allow_text_over_slots(bool allow);
-
  private:
-  struct slot {
-    std::string difficulty = "";
-    std::string owner = "";
-    bool is_mine = false;
-    uint32_t bits = 0;
-  };
-
-  std::shared_ptr<SlotsModelInterface> model;
-  uint32_t slots_number;
-  std::vector<slot> slots;
-  uint32_t initial_leading_bits = 4;
-  uint32_t min_leading_bits = 257;
-  uint32_t max_leading_bits = 0;
-  std::string owner;
-
-  uint32_t component_start_x = 50;
-  uint32_t component_start_y = 50;
-  uint32_t component_side_size_px = 600;  // will be adjusted
-  uint32_t slots_per_side;  // = sqrt(slots_number).ceil()
-  uint32_t slot_size;  // ~= component_size_size_px / squares_per_side
-  uint32_t gap;  // no gap if slots are too many
-  bool allowed_text_over_slots = true;
-
+  ValidatorGrid grid_component;
   void timerCallback() override;
-
-  uint32_t leading_bits(const std::string& s);
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DemoGrid)
 };
